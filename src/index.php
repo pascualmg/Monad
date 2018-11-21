144e684d4fdfad3bdb961d6072ba747a7f6c9d23
pascual <pascual.munoz@pccomponentes.com>
2018-11-21 02:24:35 +0100

    arasir

--- src/index.php
+++ src/index.php
@@ -5,20 +5,23 @@ require_once  '../vendor/autoload.php';
 use Reality\Just;
 use Reality\Nothing;
 
-
 $just4 = Just::of(4);
 $nothing = Nothing::of("false xD");
 
-
 var_dump($just4->value());
 
- $mapped =  $just4->map(function ($item){return $item * 2;});
-$mapped =  $mapped->map(function ($item){return $item * 2;});
-$mapped =  $mapped->map(function ($item){return $item * 2;});
-$mapped =  $mapped->map(function ($item){return $item * 2;});
-$mapped =  $mapped->map(function ($item){return $item * 2;});
-$mapped =  $mapped->map(function ($item){return $item * 2;});
+$mapped =  $just4
+  ->map(function ($item){return $item * 2;})
+  ->map(function ($item){return $item * 2;})
+  ->map(function ($item){return $item * 2;})
+  ->map(function ($item){return $item * 2;})
+  ->map(function ($item){return $item * 2;})
+  ->map(function ($item){return $item * 2;})
+  ->map(function ($item){return $item * 2;})
+  ->map(function ($item){return $item * 2;})
+  ->map(function ($item){return $item * 2;});
 
 var_dump($mapped);
+var_dump($mapped instanceof Just );
 
 
