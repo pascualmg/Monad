39494ed4ffdc9106585c326cd959258918e251a4
passh <pascual.munoz@pccomponentes.com>
2018-11-22 01:04:09 +0100

    Jisus... Christ

--- src/index.php
+++ src/index.php
@@ -2,10 +2,37 @@
 
 require_once  '../vendor/autoload.php';
 
+use FantasyLand\Monad;
+use React\EventLoop\Factory;
 use Reality\Just;
 use Reality\Maybe;
 use Reality\Nothing;
+
 use Rx\Observable;
+use Rx\Scheduler;
+
+try {
+    $loop = Factory::create();
+    Scheduler::setDefaultFactory(function () use ($loop){
+    return new Scheduler\EventLoopScheduler($loop);
+});
+} catch (\Exception $exception) {
+    print_r("No se ha podido inicial el loOps " ,$exception);
+}
+
+printf("LoOp On");
+
+$ObservableDeUnaMaybeJust = Observable::of(Maybe::of([2,3,4]))
+->subscribe('print_r');
+
+$ObservableDeUnaMaybeNothing = Observable::of(Maybe::of());
+
+Observable::interval(1000)
+    ->take(5)
+    ->flatMap(function ($i) {
+        return Observable::of($i + 1);
+    })
+    ->subscribe('print_r');
 
 $just4 = Just::of(4);
 $nothing = Nothing::of("false xD");
@@ -36,4 +63,4 @@ var_dump($mapped);
 var_dump($mapped instanceof Just );
 var_dump($mapped instanceof Maybe );
 
-
+$loop->run();
